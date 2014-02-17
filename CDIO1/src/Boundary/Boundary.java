29f23d80dae30188957e6c8068535dfdb53553ac
LidaW <lidawahidi@10.16.108.109>
2014-02-17 16:15:52 +0100

    implementeret metoder fra IBoundary til boundary

--- CDIO1/src/Boundary/Boundary.java
+++ CDIO1/src/Boundary/Boundary.java
@@ -1,5 +1,35 @@
 package Boundary;
 
-public class Boundary {
+public class Boundary implements IBoundary {
+
+	@Override
+	public String login(String oprid, String password) {
+	
+		return null;
+	}
+
+	@Override
+	public int operator() {
+		
+		return 0;
+	}
+
+	@Override
+	public int changePassword() {
+		
+		return 0;
+	}
+
+	@Override
+	public int test(String password) {
+		
+		return 0;
+	}
+
+	@Override
+	public int exit() {
+		
+		return 0;
+	}
 
 }
