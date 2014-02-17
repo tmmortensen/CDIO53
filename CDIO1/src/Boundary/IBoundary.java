d88cff5b9d20526894b9a8206e45cf2bbb59ba3e
LidaW <lidawahidi@10.16.108.109>
2014-02-17 16:10:14 +0100

    lavet interface til iboundary

--- CDIO1/src/Boundary/IBoundary.java
+++ CDIO1/src/Boundary/IBoundary.java
@@ -1,5 +1,12 @@
 package Boundary;
 
 public interface IBoundary {
+	String login(String oprid, String password);
+	int operator ();
+	int changePassword ();
+	int test( String password);
+	int exit();
+
+	
 
 }
