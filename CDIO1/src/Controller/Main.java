7f6b8be59bcbfe8509b7019527cc89d0afd30af6
BlibberBlobber <Mortenvorborg@hotmail.com>
2014-02-20 21:21:48 +0100

    Skrevet lidt videre i MainMenu

--- CDIO1/src/Controller/Main.java
+++ CDIO1/src/Controller/Main.java
@@ -1,6 +1,9 @@
 package Controller;
+
+import data.IOperatoerDAO.DALException;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DALException {
 		MainMenu mainMenu = new MainMenu();
 		mainMenu.RunMain();
 		
--- CDIO1/src/Controller/MainMenu.java
+++ CDIO1/src/Controller/MainMenu.java
@@ -1,39 +1,55 @@
 package Controller;
 
-import data.IOperatoerDAO;
+import java.util.List;
+
+import data.Data;
+import data.IOperatoerDAO.DALException;
 import data.OperatoerDTO;
+import data.Operator;
 import Boundary.Boundary;
 
 public class MainMenu {
-	String oprID,password,name;
+	String password, name;
+	int oprID;
 	Boundary boundary = new Boundary();
-	OperatoerDTO operatoerDTO = new OperatoerDTO();
-	
-	public MainMenu(){
+	Data data = new Data();
+
+	public MainMenu() {
 	}
 
-	public void RunMain(){
+	public void RunMain() throws DALException {
+
+		boundary.login();
+
+		Operator flemming = new Operator("Flemming", "FH", "367495-7483","Flemse");
+		OperatoerDTO Operatoer1 = new OperatoerDTO(14, flemming);
+
+		data.createOperatoer(Operatoer1);
+
+		List<OperatoerDTO> operatorlist = data.getOperatoerList();
 		
-		boundary.login(); 
-		Operatoer a = operatoerDTO.getOperatoer(oprID);
-		oprID = Operatoer.getID;
-		
-		
-		
-		
-		
+		for (int i = 0; i<=operatorlist.size() ; i++ ){
+			
+			if(operatorlist.get(i).getOprNavn()==name&&operatorlist.get(i).getPassword()==password){
+				
+				oprID = operatorlist.get(i).getOprId();
+				
+			}
+			
+		}
 		
 		
 		
 		
 	}
-	public String[] getLogin(){
+
+	public String[] getLogin() {
 		String[] user = new String[3];
-		user[0]=oprID;
-		user[1]=password;
-		user[2]=name; 
-		return user; 
-		
+		user[0] = oprID;
+		user[1] = password;
+		user[2] = name;
+		return user;
+
 	}
-	
+
 }
