66cbdeff29485cebe9ec9fc494a4c4e4395ea36a
Dickow <Jeppedic@hotmail.com>
2014-02-24 15:21:00 +0100

    jeg har tilføjet løkker til mainMenu

--- CDIO1/src/Controller/MainMenu.java
+++ CDIO1/src/Controller/MainMenu.java
@@ -9,8 +9,10 @@ import data.Operator;
 import Boundary.Boundary;
 
 public class MainMenu {
-	String password, name;
-	int oprID;
+	String password, oprID;
+	boolean choice = true; 
+	boolean choice1 = true; 
+	
 	Boundary boundary = new Boundary();
 	Data data = new Data();
 
@@ -18,21 +20,29 @@ public class MainMenu {
 	}
 
 	public void RunMain() throws DALException {
-
-		boundary.login();
-
+		
 		Operator flemming = new Operator("Flemming", "FH", "367495-7483","Flemse");
 		OperatoerDTO Operatoer1 = new OperatoerDTO(14, flemming);
-
 		data.createOperatoer(Operatoer1);
-
 		List<OperatoerDTO> operatorlist = data.getOperatoerList();
 		
-		for (int i = 0; i<=operatorlist.size() ; i++ ){
-			
-			if(operatorlist.get(i).getOprNavn()==name&&operatorlist.get(i).getPassword()==password){
+		
+		
+
+		// asks the user if he/she wants to try to login again if it failed
+		while(choice==true){
+			String[] login = boundary.login();
+			oprID=login[0]; login[1]=password; 
+			int opr_ID= Integer.parseInt(oprID);
+			if(data.getOperatoer(opr_ID).getPassword().equalsIgnoreCase(password)){
+				 while(choice1==true){
+					choice=false;
+					
+					
+				}
 				
-				oprID = operatorlist.get(i).getOprId();
+				}
+			else{
 				
 			}
 			
@@ -43,13 +53,6 @@ public class MainMenu {
 		
 	}
 
-	public String[] getLogin() {
-		String[] user = new String[3];
-		user[0] = oprID;
-		user[1] = password;
-		user[2] = name;
-		return user;
 
-	}
 
 }
