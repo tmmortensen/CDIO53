1e2a85767c6de177b08df2a195b2d5225b08e7c1
Dickow <Jeppedic@hotmail.com>
2014-03-05 11:59:38 +0100

    vi har ændret i det igen

--- CDIO2/src/boundary/InputBoundary.java
+++ CDIO2/src/boundary/InputBoundary.java
@@ -2,10 +2,13 @@ package boundary;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
+
+import sun.tools.jar.Main;
 
 public class InputBoundary implements IBoundary {
 
@@ -17,36 +20,36 @@ public class InputBoundary implements IBoundary {
 
 	public void run() {
 
-	}
-
-	/**
-	 * The main method that starts our client tcp connection to the server.
-	 * 
-	 * @param argv
-	 * @throws Exception
-	 */
-	public static void main(String argv[]) throws Exception {
 		String sentence;
 		String modifiedSentence;
+
+		try {
+			BufferedReader inFromUser = new BufferedReader(
+					new InputStreamReader(System.in));
+
+			Socket clientSocket = new Socket("localhost", 8000);
+
+			DataOutputStream outToServer = new DataOutputStream(
+					clientSocket.getOutputStream());
+
+			BufferedReader inFromServer = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
+			while (true) {
+
+				sentence = inFromUser.readLine();
+				outToServer.writeBytes(sentence + "\r\n");
+				modifiedSentence = inFromServer.readLine();
+				System.out.println("FROM SERVER: " + modifiedSentence);
+
+				Thread.sleep(200);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
 		inputBoundary.run();
-
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
-				System.in));
-
-		Socket clientSocket = new Socket("localhost", 8000);
-
-		DataOutputStream outToServer = new DataOutputStream(
-				clientSocket.getOutputStream());
-
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
-				clientSocket.getInputStream()));
-
-		sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + "\r\n");
-		modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
-
-		clientSocket.close();
 
 	}
 
