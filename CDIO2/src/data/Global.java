2e4d0f9c2ab29bde64ec44bbde8e0ebd5c56d033
Mikkel Westh <westh_mikkel@hotmail.com>
2014-03-10 15:38:43 +0100

    ny variabel til den streng der modtages over netværket

--- CDIO2/src/data/Global.java
+++ CDIO2/src/data/Global.java
@@ -8,8 +8,8 @@ public class Global {
 	public static boolean exit;
 	public static double tara;
 	public static double brutto;
-	public static String inLine;
 	public static String display;
+	public static String networkString;
 	public static int port;
 	public static InetAddress address;
 	public static long lastUpdate;
