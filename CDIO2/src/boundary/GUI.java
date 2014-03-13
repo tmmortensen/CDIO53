9766cbdd4425ddac4e1c8df2e4947f551cc8796e
tmmortensen <thomas.martin.mortensen@gmail.com>
2014-03-13 17:58:24 +0100

    Dynamisk GUI lavet. Nu mangler den bare funktionalitet

--- CDIO2/src/boundary/GUI.java
+++ CDIO2/src/boundary/GUI.java
@@ -1,22 +1,31 @@
 package boundary;
 
-import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextPane;
 
-public class GUI {
+public class GUI implements IBoundary {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Vægtsimulator");
-		JPanel panel = new JPanel();
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(null);
-		GridLayout grid = new GridLayout(4, 4);
-		frame.setPreferredSize(new Dimension(500, 500));
+	JFrame f = new JFrame("Vægtsimulator");
+	JPanel viewPanel = new JPanel();
+	JPanel buttonPanel = new JPanel();
+	JPanel mainPanel = new JPanel();
+	JPanel taraPanel = new JPanel();
+	EventHandler handler = new EventHandler();
 
+	@Override
+	public void run() {
+		// define components
 		JButton b0 = new JButton("0");
 		JButton b1 = new JButton("1");
 		JButton b2 = new JButton("2");
@@ -27,36 +36,66 @@ public class GUI {
 		JButton b7 = new JButton("7");
 		JButton b8 = new JButton("8");
 		JButton b9 = new JButton("9");
-		JButton b10 = new JButton("CLEAR");
-		JButton b11 = new JButton("ENTER");
+		JButton clear = new JButton("CLEAR");
+		JButton enter = new JButton("ENTER");
+		JButton tara = new JButton("<T>");
+		JTextPane toweight = new JTextPane();
+		JTextPane fromweight = new JTextPane();
+		JSpinner incDecWeight = new JSpinner();
 
-		frame.setVisible(true);
-		panel.setLayout(grid);
-		frame.pack();
-		panel2.setLayout(null);
+		// define attributes on components
+		toweight.setBackground(Color.black);
+		toweight.setForeground(Color.green);
+		fromweight.setBackground(Color.black);
+		fromweight.setSelectedTextColor(Color.green);
 
-		panel.add(b1);
-		panel.add(b2);
-		panel.add(b3);
-		panel.add(b4);
-		panel.add(b5);
-		panel.add(b6);
-		panel.add(b7);
-		panel.add(b8);
-		panel.add(b9);
-		panel.add(b10);
-		panel.add(b0);
-		panel.add(b11);
+		// set layoutmanagers
+		viewPanel.setLayout(new BoxLayout(viewPanel, BoxLayout.Y_AXIS));
+		buttonPanel.setLayout(new GridLayout(4, 4));
+		mainPanel.setLayout(new BorderLayout());
+		taraPanel.setLayout(new BoxLayout(taraPanel, BoxLayout.X_AXIS));
 
-		frame.setContentPane(panel);
+		// add components
+		buttonPanel.add(b0);
+		buttonPanel.add(b1);
+		buttonPanel.add(b2);
+		buttonPanel.add(b3);
+		buttonPanel.add(b4);
+		buttonPanel.add(b5);
+		buttonPanel.add(b6);
+		buttonPanel.add(b7);
+		buttonPanel.add(b8);
+		buttonPanel.add(b9);
+		buttonPanel.add(clear);
+		buttonPanel.add(enter);
 
-		// JButton tara = new JButton("TARA");
-		// JTextArea weight = new JTextArea(10, 2);
-		//
-		// panel2.add(tara);
-		// panel2.add(panel, BorderLayout.EAST);
-		// panel2.add(weight, BorderLayout.WEST);
-		//
-		// frame.getContentPane().add(panel2);
+		mainPanel.add(buttonPanel, BorderLayout.EAST);
+		mainPanel.add(viewPanel, BorderLayout.CENTER);
+		mainPanel.add(incDecWeight, BorderLayout.NORTH);
+
+		viewPanel.add(toweight);
+		viewPanel.add(Box.createVerticalStrut(10));
+		viewPanel.add(fromweight);
+		viewPanel.add(Box.createVerticalStrut(10));
+		viewPanel.add(taraPanel);
+
+		taraPanel.add(tara);
+		taraPanel.add(incDecWeight);
+
+		// add textPanel to Contentpane
+		f.getContentPane().add(mainPanel);
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(640, 480);
+		f.setVisible(true);
+	}
+
+	class EventHandler implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+
+		}
+
 	}
 }
