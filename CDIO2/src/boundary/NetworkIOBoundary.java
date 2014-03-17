88e16da91ceb6c8b6ce17cd2c7c68bb7b76109a0
Dickow <Jeppedic@hotmail.com>
2014-03-17 14:41:16 +0100

    Jeg har ændret i RM20 8 kommandoen

--- CDIO2/src/boundary/NetworkIOBoundary.java
+++ CDIO2/src/boundary/NetworkIOBoundary.java
@@ -12,49 +12,55 @@ public class NetworkIOBoundary implements IBoundary {
 	private DataOutputStream outstream;
 	private BufferedReader instream;
 	private IProgramState programState;
-	
-	public NetworkIOBoundary(Socket socket, IProgramState programState){
+
+	public NetworkIOBoundary(Socket socket, IProgramState programState) {
 		sock = socket;
 		this.programState = programState;
 	}
-	public void run() {	
-		try{
+
+	public void run() {
+		try {
 			instream = new BufferedReader(new InputStreamReader(
-				sock.getInputStream()));
+					sock.getInputStream()));
 			outstream = new DataOutputStream(sock.getOutputStream());
-		}
-		catch(Exception e){
-			if(programState.isRunning())
+		} catch (Exception e) {
+			if (programState.isRunning())
 				System.out.println("Exception: " + e.getMessage());
 			programState.quit();
 			return;
 		}
-		
+
 		try {
 			String netString;
-			while (!( netString = instream.readLine().toUpperCase()).isEmpty()) {
+			while (!(netString = instream.readLine().toUpperCase()).isEmpty()) {
 				programState.setNetString(netString);
 				if (netString.startsWith("RM20 8")) {
-					outstream.writeBytes("RM20 B"+ "indtast nr."+"\r\n");
-					while(netString.startsWith("RM20 8"));{}
-					outstream.writeBytes("RM20 A" + "\r\n");
-				}
-				else if(netString.startsWith("RESET")){
+					outstream.writeBytes("RM20 B");
+					programState.setDisplayText("indtast batch_nr: ");
+					while (programState.getDisplayText().isEmpty()) {
+					}
+					outstream.writeBytes("RM20 A"
+							+ programState.getDisplayText());
+
+				} else if (netString.startsWith("RESET")) {
 					programState.reset();
-					outstream.writeBytes("du har nulstillet programmet" + "\r\n");
-				}
-				else if (netString.startsWith("P111")) {
-					programState.setDisplayText(netString.substring(5,netString.length()));
-					outstream.writeBytes("P111 A"+ "\r\n");
+					outstream.writeBytes("du har nulstillet programmet"
+							+ "\r\n");
+				} else if (netString.startsWith("P111")) {
+					programState.setDisplayText(netString.substring(5,
+							netString.length()));
+					outstream.writeBytes("P111 A" + "\r\n");
 				} else if (netString.startsWith("D")) {
 					if (netString.equals("D"))
 						programState.setDisplayText("");
 					else
-						programState.setDisplayText(netString.substring(2, netString.length()));
+						programState.setDisplayText(netString.substring(2,
+								netString.length()));
 					outstream.writeBytes("DB" + "\r\n");
 				} else if (netString.startsWith("T")) {
 					programState.tare();
-					outstream.writeBytes("T " + (programState.getGross()) + " kg " + "\r\n");
+					outstream.writeBytes("T " + (programState.getGross())
+							+ " kg " + "\r\n");
 				} else if (netString.startsWith("S")) {
 					outstream.writeBytes("S " + (programState.getNet())
 							+ " kg " + "\r\n");
