12effb332636304f250913a3b2e7d9b231c3a539
MelikeY <mmelikey@gmail.com>
2014-04-01 16:18:46 +0200

    log.txt

--- WUC/src/client/WriteToFile.java
+++ WUC/src/client/WriteToFile.java
@@ -1,5 +1,24 @@
 package client;
 
-public class WriteToFile {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WriteToFile {
+	
+	public static String filename   = "log.txt";
+	
+	 public static void save(int dato, int tid, int oprNr, int varenr, int afvejning, int lager) throws FileNotFoundException {
+		 try {
+			 FileWriter fw =new FileWriter(new File(filename), true);
+			 fw.write("\n");
+			 fw.write(dato + " | " + tid + " | " + oprNr + " |"+ varenr +"|" +afvejning +"|" + lager +"|");
+			 fw.close();
+			 } catch (IOException e) {
+			 e.printStackTrace();
+		 
+		 
+			 }
+	 }
 }
