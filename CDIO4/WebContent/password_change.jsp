38b8cbcd2d9e65ec045afbaa25dbc30930e2b856
Thomas M. Mortensen <thomas.martin.mortensen@gmail.com>
2014-05-08 09:55:38 +0200

    Forsøgt mig lidt

--- CDIO4/WebContent/password_change.jsp
+++ CDIO4/WebContent/password_change.jsp
@@ -4,8 +4,8 @@
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <jsp:useBean id="error" class="java.lang.String" scope="request"/>
-<jsp:useBean id="userID" class="java.lang.String" scope="request"/>
-<jsp:useBean id="password" class="java.lang.String" scope="request"/>
+<jsp:useBean id="newPword1" class="java.lang.String" scope="request"/>
+<jsp:useBean id="newPword2" class="java.lang.String" scope="request"/>
 <jsp:useBean id="redirect" class="java.lang.String" scope="request"/>
 <html>
 <head>
@@ -19,15 +19,12 @@
 		<h1>Skift dit password her:</h1>
 		<% if (error != null){ %> <div class="error"> <% out.print(error); %> </div> <%} %>
 		<form method="post">
-			<label for="currentPassword">Nuværende password</label>
-			<input type="text" name="CurrentPassword" id="currentPassword"
-				value="<% out.print(password); %>">
-			<label for="currentPassword">Nyt password</label>
-			<input type="text" name="NewPassword" id="newPassword"
-				value="<% out.print(password); %>">
+			<label for="newPword1">Nyt password</label>
+			<input type="text" name="NewPassword1" id="newPword1"
+				value="<% out.print(newPword1); %>">
 			<label for="currentPassword">Gentag nyt password</label>
-			<input type="text" name="NewPassword2" id="newPassword2"
-				value="<% out.print(password); %>">
+			<input type="text" name="NewPassword2" id="newPword2"
+				value="<% out.print(newPword2); %>">
 			<input type="submit" value="Change">
 		</form>
 	</div>
--- CDIO4/src/controller/PasswordChangeController.java
+++ CDIO4/src/controller/PasswordChangeController.java
@@ -1,66 +1,100 @@
 package controller;
 
-import java.io.*;
+import java.io.IOException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import data.DALException;
 import data.Data;
+import data.IDataReadOnly;
 
-public class PasswordChangeController extends HttpServlet {
+public class PasswordChangeController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
-
-	Data data;
-
-	public void init(ServletConfig config) throws ServletException {
+	IDataReadOnly data;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException{
 		super.init(config);
-		data = (Data) getServletContext().getAttribute("data");
-		if (data == null) {
-			data = new Data();
-			getServletContext().setAttribute("data", data);
+		data = (IDataReadOnly) getServletContext().getAttribute("data");
+		if (data == null){
+			Data newdata = new Data();
+			getServletContext().setAttribute("data", newdata);
+			data = newdata;
 		}
 	}
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String err = "";
-		//User user = (User) request.getSession().getAttribute("user");
-
-		Integer uid = Integer.parseInt(request.getParameter("UserID"));
-		String password = request.getParameter("password");
-
-		if (uid != null) {
-			try {
-				data.getOperatoer(uid);
-				request.setAttribute("userID", uid);
-				RequestDispatcher dispatcher = getServletContext()
-						.getRequestDispatcher("/edit_user.jsp");
-				dispatcher.forward(request, response);
-
-			} catch (DALException e) {
-				err = e.getMessage();
-
-			}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+		
+		User user = (User) request.getSession().getAttribute("user");
+		if (user == null){
+			user = new User();
+			user.init(data);
+			request.getSession().setAttribute("user", user);
 		}
 		
-		if(uid!=null && password!= null){
-			
-			
-			
-			
+		String passwordError = "";
+		
+		if (!user.isinitialized()){
+			user.init(data);
 		}
 
+		String redirect;
+		if (request.getParameter("redirect") != null)
+			redirect = request.getParameter("redirect");
+		else
+			redirect = "mainmenu";
+
+		String curPword = request.getParameter("curPword");
+		String newPword1 = request.getParameter("newPword1");
+		String newPword2 = request.getParameter("newPword2");
+
+		String logout = request.getParameter("logout");
+		if (logout != null && logout.toLowerCase().equals("true")){
+			user.logout();
+		}
+
+		if (user.isLoggedIn()){
+			response.sendRedirect(redirect);
+			return;
+		}
+
+		if (newPword1 != null && newPword2 != null)
+			try {
+				if (newPword1.equals(newPword2)){
+					response.sendRedirect(redirect);
+					return;
+				} else{
+					passwordError = "De indtastede password matcher ikke";	
+				}
+			} catch (Exception e){
+				passwordError = "Password overholder ikke gældende regler";	
+			}
+		else {
+			newPword1 = "";
+			newPword2 = "";
+		}
+		
+		// TODO fjern dette
 		request.setAttribute("data", data);
-		request.setAttribute("error", err);
-		RequestDispatcher dispatcher = getServletContext()
-				.getRequestDispatcher("/edit_user.jsp");
+		// ^^ er kun til test
+		
+		request.setAttribute("error", passwordError);
+		request.setAttribute("userId", curPword);
+		request.setAttribute("newPword1", newPword1);
+		request.setAttribute("newPword2", newPword2);
+		request.setAttribute("redirect", redirect);
+		
+		
+		RequestDispatcher dispatcher =
+				getServletContext().getRequestDispatcher("/password_change.jsp");
 		dispatcher.forward(request, response);
 	}
-
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
 		doPost(request, response);
 	}
 }
