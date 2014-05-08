fd67a2e460f14e45e852756ad4b1b766fc6dffe5
Dickow <Jeppedic@hotmail.com>
2014-05-08 21:14:32 +0200

    jeg har lavet helt om på det nu, håber det er for det bedre

--- CDIO4/WebContent/edit_user.jsp
+++ CDIO4/WebContent/edit_user.jsp
@@ -3,10 +3,12 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<jsp:useBean id="error" class="java.lang.String" scope="request"/>
-<jsp:useBean id="userID" class="java.lang.String" scope="request"/>
-<jsp:useBean id="password" class="java.lang.String" scope="request"/>
-<jsp:useBean id="redirect" class="java.lang.String" scope="request"/>
+<jsp:useBean id="error"    class="java.lang.String" scope="request"/>
+<jsp:useBean id="userID"   class="java.lang.String" scope="request"/>
+<jsp:useBean id="userINI"  class="java.lang.String" scope="request"/>
+<jsp:useBean id="userNAME" class="java.lang.String" scope="request"/>
+<jsp:useBean id="userCPR"  class="java.lang.String" scope="request"/>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
@@ -15,28 +17,35 @@
 </head>
 <body>
 
+	
 <div class="edit_user">
-		<h1>Intast bruger ID for den du ønsker at ændre </h1>
+		<h1>Indtast det nye brugerID </h1>
 		<% if (error != null){ %> <div class="error"> <% out.print(error); %> </div> <%} %>
 		<form method="post">
 			<label for="userid">Bruger ID</label>
-			<input type="text" name="UserID" id="userid"
+			<input type="text" name="userID" id="userid"
 				value="<% out.print(userID); %>">
+				
+				<label for="userini">Bruger ini</label>
+			<input type="text" name="userINI" id="userini"
+				value="<% out.print(userINI); %>">
+				
+				<label for="username">Bruger Navn</label>
+			<input type="text" name="userNAME" id="username"
+				value="<% out.print(userNAME); %>">
+				
+				<label for="usercpr">CPR</label>
+			<input type="text" name="userCPR" id="usercpr"
+				value="<% out.print(userCPR); %>">
+				
 			<input type="submit" value="Edit">
 		</form>
 	</div>
 
 
 
-<%
-Data data =(Data) request.getAttribute("data");
-List<OperatoerDTO> operators = data.getOperatoerList();
 
-for(OperatoerDTO op: operators){
-	out.print("Bruger ID: " + op.getOprId());	
-}
 
-%>
 
 
 
--- CDIO4/src/controller/EditUserController.java
+++ CDIO4/src/controller/EditUserController.java
@@ -7,12 +7,15 @@ import javax.servlet.http.*;
 
 import data.DALException;
 import data.Data;
+import data.OperatoerDTO;
+import data.UserInfo;
 
 public class EditUserController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	Data data;
+	UserInfo userinfo = (UserInfo) getServletContext().getAttribute("user");
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
@@ -26,34 +29,58 @@ public class EditUserController extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String err = "";
-		User user = (User) request.getSession().getAttribute("user");
+		String uid = request.getParameter("userID");
+		String ini = request.getParameter("userINI");
+		String name = request.getParameter("userNAME");
+		String cpr = request.getParameter("userCPR");
 
-		Integer uid = Integer.parseInt(request.getParameter("UserID"));
-		String password = request.getParameter("password");
+		if (uid.equals(userinfo.id) && ini.equals(userinfo.ini)
+				&& name.equals(userinfo.name) && cpr.equals(userinfo.cpr)) {
+			err = "du har ikke ændret noget!";
+			request.setAttribute("userID", uid);
+			request.setAttribute("userINI", ini);
+			request.setAttribute("userNAME", name);
+			request.setAttribute("userCPR", cpr);
+			request.setAttribute("error", err);
+			RequestDispatcher dispatcher = getServletContext()
+					.getRequestDispatcher("/edit_user.jsp");
+			dispatcher.forward(request, response);
 
-		if (uid != null) {
-			try {
-				data.getOperatoer(uid);
-				request.setAttribute("userID", uid);
-				RequestDispatcher dispatcher = getServletContext()
-						.getRequestDispatcher("/edit_user.jsp");
-				dispatcher.forward(request, response);
+		} else {
+			int Uid = Integer.parseInt(uid);
 
-			} catch (DALException e) {
-				err = e.getMessage();
+			if (Uid == userinfo.id) {
+				try {
+					String password = data.getOperatoer(userinfo.id)
+							.getPassword();
+					data.deleteOperatoer(Uid);
+					data.createOperatoer(new OperatoerDTO(Uid, ini, name, cpr,
+							password));
+
+				} catch (DALException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					String password = data.getOperatoer(userinfo.id)
+							.getPassword();
+					data.deleteOperatoer(userinfo.id);
+					data.createOperatoer(new OperatoerDTO(Uid, ini, name, cpr,
+							password));
+
+				} catch (DALException e) {
+					e.printStackTrace();
+				}
 
 			}
 		}
-		
-		if(uid!=null && password!= null){
-			
-			
-			
-			
-		}
 
-		request.setAttribute("data", data);
-		request.setAttribute("error", err);
+		request.setAttribute("userID", uid);
+		request.setAttribute("userINI", ini);
+		request.setAttribute("userNAME", name);
+		request.setAttribute("userCPR", cpr);
+
 		RequestDispatcher dispatcher = getServletContext()
 				.getRequestDispatcher("/edit_user.jsp");
 		dispatcher.forward(request, response);
