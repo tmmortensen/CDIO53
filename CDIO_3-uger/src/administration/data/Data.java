2282a92e4d969256e58be54e68041b9fc8a88e3b
tmmortensen <thomas.martin.mortensen@gmail.com>
2014-06-05 21:08:40 +0200

    Manglede connection til database i en enkelt metode

--- CDIO_3-uger/src/administration/data/Data.java
+++ CDIO_3-uger/src/administration/data/Data.java
@@ -59,7 +59,7 @@ public class Data implements IOperatoerDAO {
 				System.out.print(", First: " + first);
 				System.out.println(", Last: " + last);
 			}
-			// STEP 6: Clean-up environment
+			// Clean-up environment
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -96,17 +96,7 @@ public class Data implements IOperatoerDAO {
 	@Override
 	public synchronized OperatoerDTO getOperatoer(int oprId)
 			throws DALException {
-		if (oprId < 1 || oprId > 99999999)
-			throw new DALException("Id out of bounds");
-		Operator operator = operators.get(oprId);
-		if (operator == null)
-			throw new DALException("Id not found");
-		return new OperatoerDTO(oprId, operator);
-	}
-
-	@Override
-	public List<OperatoerDTO> getOperatoerList() throws DALException {
-		// creating the sysadmin
+		// get operator
 		try {
 			// Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
@@ -136,7 +126,72 @@ public class Data implements IOperatoerDAO {
 				System.out.print(", First: " + first);
 				System.out.println(", Last: " + last);
 			}
-			// STEP 6: Clean-up environment
+			// Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
+		if (oprId < 1 || oprId > 99999999)
+			throw new DALException("Id out of bounds");
+		Operator operator = operators.get(oprId);
+		if (operator == null)
+			throw new DALException("Id not found");
+		return new OperatoerDTO(oprId, operator);
+	}
+
+	@Override
+	public List<OperatoerDTO> getOperatoerList() throws DALException {
+		// Get operator list
+		try {
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, first, last, age FROM Employees";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", Age: " + age);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// Clean-up environment
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -171,7 +226,7 @@ public class Data implements IOperatoerDAO {
 	@Override
 	public synchronized void createOperatoer(OperatoerDTO opr)
 			throws DALException {
-		// creating the sysadmin
+		// creating the operator
 		try {
 			// Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
@@ -201,7 +256,7 @@ public class Data implements IOperatoerDAO {
 				System.out.print(", First: " + first);
 				System.out.println(", Last: " + last);
 			}
-			// STEP 6: Clean-up environment
+			// Clean-up environment
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -239,7 +294,7 @@ public class Data implements IOperatoerDAO {
 	public synchronized void updateOperatoer(OperatoerDTO opr)
 			throws DALException {
 
-		// creating the sysadmin
+		// Update operator
 		try {
 			// Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
@@ -269,7 +324,7 @@ public class Data implements IOperatoerDAO {
 				System.out.print(", First: " + first);
 				System.out.println(", Last: " + last);
 			}
-			// STEP 6: Clean-up environment
+			// Clean-up environment
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -313,7 +368,7 @@ public class Data implements IOperatoerDAO {
 
 	@Override
 	public synchronized void deleteOperatoer(int id) throws DALException {
-		// creating the sysadmin
+		// Delete operator
 		try {
 			// Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
@@ -343,7 +398,7 @@ public class Data implements IOperatoerDAO {
 				System.out.print(", First: " + first);
 				System.out.println(", Last: " + last);
 			}
-			// STEP 6: Clean-up environment
+			// Clean-up environment
 			rs.close();
 			stmt.close();
 			conn.close();
