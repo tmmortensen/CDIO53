d4e85c37dc61d48177977ccf1620ae03df6d7ed8
tmmortensen <thomas.martin.mortensen@gmail.com>
2014-06-05 14:18:22 +0200

    Leget lidt med adgang til database

--- CDIO_3-uger/src/administration/data/Data.java
+++ CDIO_3-uger/src/administration/data/Data.java
@@ -1,27 +1,101 @@
 package administration.data;
 
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Data implements IOperatoerDAO {
 
 	Map<Integer, Operator> operators;
 	int adminCount;
 
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/weight";
+
+	// Database credentials
+	static final String USER = "root";
+	static final String PASS = "";
+
+	Connection conn = null;
+	Statement stmt = null;
+
 	public Data() {
-		operators = new HashMap<Integer, Operator>();
-		adminCount = 0;
-		
+		// operators = new HashMap<Integer, Operator>();
+		// adminCount = 0;
+
 		// creating the sysadmin
 		try {
-		createOperatoer(new OperatoerDTO(1,"Sysadmin", "SA", "1234567890", "adminpw",true));
-		} catch (Exception e) {}
-		
-		//TODO remove this when done testing
-		createDefaultOperators();
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, first, last, age FROM Employees";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", Age: " + age);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
+		// createOperatoer(new OperatoerDTO(1, "Sysadmin", "SA", "1234567890",
+		// "adminpw", true));
+		// } catch (Exception e) {
+		// }
+		//
+		// // TODO remove this when done testing
+		// createDefaultOperators();
 	}
 
 	@Override
-	public synchronized OperatoerDTO getOperatoer(int oprId) throws DALException {
+	public synchronized OperatoerDTO getOperatoer(int oprId)
+			throws DALException {
 		if (oprId < 1 || oprId > 99999999)
 			throw new DALException("Id out of bounds");
 		Operator operator = operators.get(oprId);
@@ -32,6 +106,61 @@ public class Data implements IOperatoerDAO {
 
 	@Override
 	public List<OperatoerDTO> getOperatoerList() throws DALException {
+		// creating the sysadmin
+		try {
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, first, last, age FROM Employees";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", Age: " + age);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
 		List<OperatoerDTO> returnlist = new ArrayList<OperatoerDTO>();
 		for (Map.Entry<Integer, Operator> entry : operators.entrySet()) {
 			returnlist.add(new OperatoerDTO(entry.getKey(), entry.getValue()));
@@ -40,7 +169,63 @@ public class Data implements IOperatoerDAO {
 	}
 
 	@Override
-	public synchronized void createOperatoer(OperatoerDTO opr) throws DALException {
+	public synchronized void createOperatoer(OperatoerDTO opr)
+			throws DALException {
+		// creating the sysadmin
+		try {
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, first, last, age FROM Employees";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", Age: " + age);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
 		Operator result = operators.put(opr.getOprId(), new Operator(opr));
 		if (result != null) {
 			operators.put(opr.getOprId(), result);
@@ -51,55 +236,175 @@ public class Data implements IOperatoerDAO {
 	}
 
 	@Override
-	public synchronized void updateOperatoer(OperatoerDTO opr) throws DALException {
+	public synchronized void updateOperatoer(OperatoerDTO opr)
+			throws DALException {
+
+		// creating the sysadmin
+		try {
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, first, last, age FROM Employees";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", Age: " + age);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
 		Operator result = operators.put(opr.getOprId(), new Operator(opr));
 		if (result == null) {
 			operators.remove(opr.getOprId());
 			throw new DALException("Operator ID does not exist.");
 		}
-		if (!opr.isAdmin() && result.isAdmin()){
-			if(adminCount == 1){
+		if (!opr.isAdmin() && result.isAdmin()) {
+			if (adminCount == 1) {
 				operators.put(opr.getOprId(), result);
-				throw new DALException("Kan ikke fjerne administratorrettighedder fra den sidste administrator");
+				throw new DALException(
+						"Kan ikke fjerne administratorrettighedder fra den sidste administrator");
 			} else
 				adminCount--;
 		}
 		if (opr.isAdmin() && !result.isAdmin())
 			adminCount++;
 	}
-	
+
 	@Override
-	public synchronized void deleteOperatoer(int id)throws DALException{
+	public synchronized void deleteOperatoer(int id) throws DALException {
+		// creating the sysadmin
+		try {
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, first, last, age FROM Employees";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id1 = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+
+				// Display values
+				System.out.print("ID: " + id1);
+				System.out.print(", Age: " + age);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
 		if (id < 1 || id > 99999999)
 			throw new DALException("Id out of bounds");
 		Operator result = operators.remove(id);
 		if (result == null)
 			throw new DALException("Operator does not exist");
-		if (result.isAdmin()){
-			if(adminCount == 1){
+		if (result.isAdmin()) {
+			if (adminCount == 1) {
 				operators.put(id, result);
-				throw new DALException("Kan ikke fjerne den sidste administrator");
+				throw new DALException(
+						"Kan ikke fjerne den sidste administrator");
 			} else
 				adminCount--;
 		}
 	}
-	
-	public void createDefaultOperators(){
+
+	public void createDefaultOperators() {
 		try {
-			OperatoerDTO newOp = new OperatoerDTO(11,"Test Guy", "TG", "1234567890", OperatoerDTO.generatePassword());
+			OperatoerDTO newOp = new OperatoerDTO(11, "Test Guy", "TG",
+					"1234567890", OperatoerDTO.generatePassword());
 			createOperatoer(newOp);
 
-			newOp = new OperatoerDTO(12,"Test Guy 2", "TG2", "1234567890", OperatoerDTO.generatePassword());
+			newOp = new OperatoerDTO(12, "Test Guy 2", "TG2", "1234567890",
+					OperatoerDTO.generatePassword());
 			createOperatoer(newOp);
-		
-			newOp = new OperatoerDTO(13,"Test Guy 3", "TG3", "1234567890", OperatoerDTO.generatePassword());
+
+			newOp = new OperatoerDTO(13, "Test Guy 3", "TG3", "1234567890",
+					OperatoerDTO.generatePassword());
 			createOperatoer(newOp);
-		
-			newOp = new OperatoerDTO(14,"Test Guy 4", "TG4", "1234567890", OperatoerDTO.generatePassword());
+
+			newOp = new OperatoerDTO(14, "Test Guy 4", "TG4", "1234567890",
+					OperatoerDTO.generatePassword());
 			createOperatoer(newOp);
-		
-			newOp = new OperatoerDTO(10,"Admin", "AM", "1234567890", "adminpw",true);
+
+			newOp = new OperatoerDTO(10, "Admin", "AM", "1234567890",
+					"adminpw", true);
 			createOperatoer(newOp);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 	}
 }
