1e144769a5c2cc2b782ed64a5beae61f1aa7304f
tmmortensen <thomas.martin.mortensen@gmail.com>
2014-06-09 12:34:27 +0200

    Recept startet

--- CDIO_3-uger/admin/admin/data/IPrescritpionDAO.java
+++ CDIO_3-uger/admin/admin/data/IPrescritpionDAO.java
@@ -1,5 +1,18 @@
 package admin.data;
 
+import java.util.List;
+
 public interface IPrescritpionDAO {
+
+	public void PrescriptionDTO() throws DALException;
+
+	public void createPrescription(PrescriptionDTO prescription)
+			throws DALException;
+
+	public void getPrescription(PrescriptionDTO prescription)
+			throws DALException;
+
+	public List<PrescriptionDTO> getAllPrescription(PrescriptionDTO prescription)
+			throws DALException;
 
 }
