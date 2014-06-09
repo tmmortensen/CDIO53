a6895bfb9ff3ec9cb0cbe64f9844a8812e7a06f5
Dickow <Jeppedic@hotmail.com>
2014-06-09 12:01:19 +0200

    flere SQL kald tilføjet nu til råvarer

--- CDIO_3-uger/admin/admin/data/CommodityData.java
+++ CDIO_3-uger/admin/admin/data/CommodityData.java
@@ -1,5 +1,52 @@
 package admin.data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommodityData implements ICommodityDAO {
+
+	@Override
+	public synchronized CommodityDTO getCommodity(int commodity_id) throws DALException {
+		ResultSet rs = Connector
+				.doQuery("SELECT * FROM user WHERE commodity_id = "
+						+ commodity_id + ";");
+		try {
+			if (!rs.first()) {
+				throw new DALException("the commodity with the id = "
+						+ commodity_id + " does not exist");
+			}
+			return new CommodityDTO(rs.getInt("commodity_id"),
+					rs.getString("commodity_name"), rs.getString("supplier"));
+		} catch (SQLException e) {
+			throw new DALException(e);
+		}
+	}
+
+	@Override
+	public synchronized List<CommodityDTO> getComList() throws DALException {
+		List<CommodityDTO> list = new ArrayList<CommodityDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM user;");
+		try {
+			while (rs.next()) {
+				list.add(new CommodityDTO(rs.getInt("commodity_id"), rs
+						.getString("commodity_name"), rs.getString("supplier")));
+			}
+		} catch (SQLException e) {
+			throw new DALException(e);
+		}
+		return list;
+
+	}
+
+	@Override
+	public synchronized void createCommodity(CommodityDTO commodity) throws DALException {
+		Connector.doUpdate("INSERT INTO user VALUES ( commodity_id = "
+				+ commodity.getComId() + " commodity_name = "
+				+ commodity.getComName() + " supplier = "
+				+ commodity.getSupplier() + ");");
+
+	}
 
 }
