370e3d26536ad8ed0255d62f9ac97d9a0a8b76e7
Dickow <Jeppedic@hotmail.com>
2014-06-09 12:32:17 +0200

    lidt flere metoder og lidt mere på DTO klasserne

--- CDIO_3-uger/admin/admin/data/ICommodityDAO.java
+++ CDIO_3-uger/admin/admin/data/ICommodityDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 public interface ICommodityDAO {
 
 	public CommodityDTO getCommodity(int commodity_id) throws DALException;
-	
+
 	public List<CommodityDTO> getComList() throws DALException;
-	
+
 	public void createCommodity(CommodityDTO commodity) throws DALException;
-	
+
 }
