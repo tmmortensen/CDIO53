bfbee13f0af5a351da4be1c369dbce190f55824d
Dickow <Jeppedic@hotmail.com>
2014-06-10 10:58:45 +0200

    flere metoder rettet til

--- CDIO_3-uger/admin/admin/data/PrescriptionData.java
+++ CDIO_3-uger/admin/admin/data/PrescriptionData.java
@@ -13,20 +13,32 @@ public class PrescriptionData implements IPrescritpionDAO {
 	}
 
 	@Override
-	public void createPrescription(PrescriptionDTO prescription)
+	public synchronized void createPrescription(PrescriptionDTO prescription)
 			throws DALException {
+		try {
+			Connector.connect();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		Connector.doUpdate("INSERT INTO prescription VALUES ( "
 				+ " prescription_id = " + prescription.getPrescriptionId()
-				+ " prescription_name = " + prescription.getPrescriptionName()
-				+ ");");
+				+ ", prescription_name = '" + prescription.getPrescriptionName()
+				+ "');");
+		Connector.closeConnection();
 	}
 
 	@Override
-	public PrescriptionDTO getPrescription(int prescriptionId)
+	public synchronized PrescriptionDTO getPrescription(int prescriptionId)
 			throws DALException {
+		try {
+			Connector.connect();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		ResultSet rs = Connector
 				.doQuery("SELECT * FROM prescription WHERE prescription_id = "
 						+ prescriptionId + ";");
+		Connector.closeConnection();
 		try {
 			if (!rs.first()) {
 				throw new DALException("the commodity with the id = "
@@ -40,10 +52,16 @@ public class PrescriptionData implements IPrescritpionDAO {
 	}
 
 	@Override
-	public List<PrescriptionDTO> getAllPrescription(PrescriptionDTO prescription)
+	public synchronized List<PrescriptionDTO> getAllPrescription(PrescriptionDTO prescription)
 			throws DALException {
+		try {
+			Connector.connect();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		List<PrescriptionDTO> list = new ArrayList<PrescriptionDTO>();
 		ResultSet rs = Connector.doQuery("SELECT * FROM prescription;");
+		Connector.closeConnection();
 		try {
 			while (rs.next()) {
 				list.add(new PrescriptionDTO(rs.getInt("prescription_id"), rs
