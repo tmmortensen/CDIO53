63b431be523be4d2532740f604ec4274de1251d3
DreamSpawn <westh_mikkel@hotmail.com>
2014-06-12 15:54:46 +0200

    commodity edit

---
+++ CDIO_3-uger/WebContent/commodity_edit_boundary.jsp
@@ -0,0 +1,81 @@
+<%@ page language="java" import="java.util.*,admin.data.*" 
+	contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<jsp:useBean id="majorError" class="java.lang.String" scope="request"/>
+<jsp:useBean id="idError" class="java.lang.String" scope="request"/>
+<jsp:useBean id="nameError" class="java.lang.String" scope="request"/>
+<jsp:useBean id="supError" class="java.lang.String" scope="request"/>
+<jsp:useBean id="newId" class="java.lang.String" scope="request"/>
+<jsp:useBean id="commodity" class="admin.data.CommodityDTO" scope="request"/>
+<% boolean complete = (Boolean) request.getAttribute("complete"); %>
+<% boolean create = (Boolean) request.getAttribute("create"); %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Edit user</title>
+</head>
+<body>
+
+	
+<div class="dialog">
+	<% if (complete){ %>
+	<%   if (!create){ %>  
+	<h1>Ændring af råvare foretaget</h1>
+	<%   } else { %>
+	<h1>Oprettelse af råvare foretaget</h1>
+	<%   } %>
+	<table border="1"><tbody>
+		<tr>
+			<th>RåvareID</th>
+			<th>Råvarenavn</th>
+			<th>Leverandør</th>
+		</tr>
+		<tr>
+		<%
+			out.println("\t\t\t\t\t<td>" + commodity.getComId() + "</td>\n");
+			out.println("\t\t\t\t\t<td>" + commodity.getComName() + "</td>\n");
+			out.println("\t\t\t\t\t<td>" + commodity.getSupplier() + "</td>\n");
+		%>
+		</tr>
+	</tbody></table>
+	<%	} else if (!majorError.equals("")){
+			if (create) {%>	<h1>Fejl under oprettelse af råvare!</h1>
+	<% 		} else { %><h1>Fejl under redigering af råvare!</h1> <% } %>
+	<div class="error"><%out.print(majorError);%></div> 
+	<% } else { %>
+	<h1>Indtast nye råvareoplysninger</h1>
+	<form method="post">
+		<% if (!idError.equals("")){ %> 
+		<div class="error"><%out.print(idError);%></div>
+		<% } %>
+		<label for="comId">Råvare ID</label>
+		<input type="text" name="newId" id="comId"
+			value="<%out.print(newId);%>">
+			
+		<% if (nameError != null && !nameError.equals("")){	%>
+		<div class="error"><%out.print(nameError);%></div>
+		<% } %>
+		<label for="comName">Råvarenavn</label>
+		<input type="text" name="newName" id="comName"
+			value="<%out.print(commodity.getComName());%>">
+			
+		<% if (supError != null && !supError.equals("")){ %>
+		<div class="error"><%out.print(supError);%></div>
+		<% } %>
+		<label for="supplier">Leverandør</label>
+		<input type="text" name="newSupplier" id="supplier"
+			value="<%out.print(commodity.getSupplier());%>">
+			
+		<% if (create) {%>	<input type="submit" value="Opret">
+		<% } else { %><input type="submit" value="Rediger"> <% } %>
+	
+	</form>
+	<%} %>
+		<div class="buttons">
+			<A href="commodity_admin">Tilbage</A>
+			<A href="login?logout=true">Log ud</A>
+		</div>
+</div>
+</body>
+</html>
--- CDIO_3-uger/admin/admin/controller/CommodityEditController.java
+++ CDIO_3-uger/admin/admin/controller/CommodityEditController.java
@@ -2,10 +2,13 @@ package admin.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import admin.data.CommodityDTO;
+import admin.data.DALException;
 import admin.data.UserType;
 
 public class CommodityEditController extends AbstractController {
@@ -20,5 +23,127 @@ public class CommodityEditController extends AbstractController {
 	@Override
 	public void doRequest(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException{
+
+		int iNewId = 0;
+		boolean isNew = false;
+		boolean anyError = false;
+		String majorError = "";
+		String idError = "";
+		String nameError = "";
+		String supError = "";
+		String sNewId = "";
+		String newName = "";
+		String newSupplier = "";
+		CommodityDTO commodity = null;
+		
+		String sComId = request.getParameter("id");
+		int iOldId = 0;
+
+		if (sComId != null && sComId.equals("new")) {
+			isNew = true;
+		} else {
+			try {
+				iOldId = Integer.parseInt(sComId);
+			} catch (NumberFormatException e) {
+				majorError = "RåvareId er ugyldigt";
+				anyError = true;
+			}
+		}
+		
+		if (isNew) {
+			commodity = new CommodityDTO();
+		} else {
+			try {
+				commodity = commodities.getCommodity(iOldId);
+				sNewId = sComId;
+			} catch (DALException e1) {
+				majorError = "Der findes ingen bruger med det angivne id";
+				anyError = true;
+			}
+		}
+		
+		if (! anyError && request.getMethod().equals("POST")) {
+			sNewId = request.getParameter("newId");
+			newName = request.getParameter("newName");
+			newSupplier = request.getParameter("newSupplier");
+			
+			try {
+				iNewId = Integer.parseInt(sNewId);
+				commodity.setCommodity_id(iNewId);
+			} catch (NumberFormatException e) {
+				idError = "Det indtastede bruger id er ikke et tal";
+				anyError = true;
+			} catch (DALException e) {
+				idError = e.getMessage();
+				anyError = true;
+			}
+			
+			try {
+				commodity.setCommodity_name(newName);
+			} catch (DALException e) {
+				nameError = e.getMessage();
+				anyError = true;
+			}
+
+			try {
+				commodity.setSupplier(newSupplier);
+			} catch (DALException e) {
+				supError = e.getMessage();
+				anyError = true;
+			}
+			
+			if (!anyError){
+				if(isNew){
+					try{
+						commodities.createCommodity(commodity);
+					} catch (DALException e) {
+						idError = e.getMessage();
+						anyError = true;
+					}
+				} else if (iOldId == iNewId) {
+					try{
+						commodities.updateCommodity(commodity);
+					} catch (DALException e) {
+						idError = e.getMessage();
+						anyError = true;
+					}
+				} else {
+					try{
+						commodities.createCommodity(commodity);
+						try{
+							commodities.deleteCommodity(iOldId);
+						} catch (DALException e) {
+							idError = "Råvare id kunne ikke ændres <BR>";
+							idError += e.getMessage();
+							anyError = true;
+							commodities.deleteCommodity(iNewId);
+						}
+					} catch (DALException e) {
+						idError = e.getMessage();
+						anyError = true;
+					}
+				}
+			}
+
+		}
+
+		request.setAttribute("majorError", majorError);
+		request.setAttribute("idError", idError);
+		request.setAttribute("nameError", nameError);
+		request.setAttribute("supError", supError);
+		
+		request.setAttribute("create", isNew);
+		request.setAttribute("complete", !anyError
+				&& request.getMethod().equals("POST"));
+
+		request.setAttribute("newId", sNewId);
+		request.setAttribute("commodity", commodity);
+		
+		
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher("/commodity_edit_boundary.jsp");
+		dispatcher.forward(request, response);
+
+
 	}
 }
