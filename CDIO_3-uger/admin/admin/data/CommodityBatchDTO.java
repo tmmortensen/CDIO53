3bb03225f9f61ac5f1003f0e6640f600ce54ffc1
tmmortensen <thomas.martin.mortensen@gmail.com>
2014-06-14 20:14:07 +0200

    Slettet constructor der var blevet oprettet dobbelt i commoditybatch

--- CDIO_3-uger/admin/admin/data/CommodityBatchDTO.java
+++ CDIO_3-uger/admin/admin/data/CommodityBatchDTO.java
@@ -27,13 +27,6 @@ public class CommodityBatchDTO {
 		setCommodityId(commodityId);
 	}
 
-	/**
-	 * empty contructor with no error checks
-	 */
-	public CommodityBatchDTO() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public int getCommodityBatchId() {
 		return commodityBatchId;
 	}
