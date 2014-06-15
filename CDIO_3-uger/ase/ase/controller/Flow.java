875d2c95af0768e32a13d98f1b94b3fd12c09f83
Nikolaj <Nikobirck@hotmailcom>
2014-06-15 11:52:19 +0200

    Mere til ASE controller. Ikke færdig endnu.

--- CDIO_3-uger/ase/ase/controller/Flow.java
+++ CDIO_3-uger/ase/ase/controller/Flow.java
@@ -2,86 +2,113 @@ package ase.controller;
 
 public class Flow implements Runnable {
 
-	private boolean state_done, done = false, sub_step_done;
+	private boolean state_done, done = false, sub_step_done, confirm = false;
 	private boolean step_done = false;
-	private boolean step2_done = false;
+	private boolean step2_done = false, step3_done = false, step4_done = false, step5_done = false, step6_done = false;
+	private boolean step7_done;
 	public void run() {
 
 		while (!done) {
-			boolean reset = false;
+			boolean reset = false; // Bliver sat til true, hvis vi ønsker at
+									// genstarte.
 			state_done = false;
 
-			while (!reset && !state_done) {
-				// bound.getID();
-				// username = checkID();
-				// confirm = bound.send(username);
-				// if(continues)
-				// step_done = true;
+			while (!reset && !state_done) { //Step 1
+				bound.getID();
+				String username = DB.checkName();
+				bound.sendName(username);
+				confirm = bound.send(username);
+				if (confirm)
+					step_done = true;
+				else
+					continue;
 
 			}
-			
+
 			state_done = false;
-			
-			while(!reset && !state_done && step_done){ //Produktbatch input state
-//				bound.getPBID();
-//				Indsæt punkt her der tjekker på produktbatch
-//				if(fail)
-//					if(prøv et andet?)
-//						continue;
-//					else
-//						reset = true;
-//						break;
-//				else
-//					step2_done = true;
+
+			while (!reset && !state_done && step_done) { // step 2 Produktbatch input
+															// state
+				int produktBatchID = bound.getProduktBatchID();
+				if (!DB.checkProduktBatchID(produktBatchID)) {// Punktet her
+																// skal tjekke
+																// på
+																// produktbatch
+					if (bound.retry())
+						continue;
+					else
+						reset = true;
+					break;
+				} else
+					step_done = false;
+				step2_done = true;
+			}
+
+			while (!reset && !state_done && step2_done) { // step 3. Raavarebatch input
+															// state
+				boolean result = bound.zero(); // I boundary skal operatøren
+												// bedes om at tømme vægten og
+												// trykke "ok", og derefter
+												// tareres vægten
+				if (!result) {
+					reset = true;
+					step2_done = false;
+				}
+				double t = bound.tara(); // Operatøren bedes at placeres tara og
+											// trykke ok.
+				step2_done = false;
+				step3_done = true;
+			}
+			while (!reset && !state_done && step3_done) {// step 4. Finde receptkomponent og huske den.				
+				step3_done = false;
+				step4_done = true;
+			}
+			while(!reset && !state_done && step4_done){// step 5. Indtast på varebatch ID state
+				DB.getRaavareID();
+				bound.outRaavareID();
+				int id = bound.getRaavareBatchID();
+					if(DB.checkRaavareBatchID(id)){
+						if(bound.retry)
+							continue;
+						else 
+							break;
+							reset = true;
+					}
+					else{
+						step4_done = false;
+						step5_done = true;
+					}
 			}
 			
-			while(!reset && !state_done && step2_done){ //Raavarebatch input state
-//				result = bound.zero(); //I boundary skal operatøren bedes om at tømme vægten og trykke "ok", og derefter tareres vægten
-//					if(result != succes){
-//						reset = true;
-//						step_done = true;
-//					}
-//				double t = bound.tara() //Operatøren bedes at placeres tara og trykke ok.
-				//Skal checkes som før.
-				
-//				Her skal finde receptkomponent og huske den.
-				sub_step_done = false;
+			
+			while(!reset && !state_done && step5_done){ //step 6
+				double n = b.getNetto(double netto, double tolerance); //På de værdier vi får fra step4.
+//				En sammenligning fra værdien på vægten og det vi har fra databasen.
+					if(fail)
+						if(bound.retry)
+							continue;
+						else{
+							reset = true;
+							step5_done = false;
+						}
+					else{
+					step5_done = false;
+					step6_done = true;
+					}
+			}
+			while(!reset && !state_done && step6_done){ //Step 7. Step hvor data gemmes.
+				DB.saveData();
+				step6_done = false;
+				step7_done = true;
 			}
 			
-			while(!reset && !state_done && !sub_step_done){//Indtast på varebatch ID state
-//				DB.getRaavareID();
-//				bound.outRaavareID();
-//				int id = bound.getRaavareBatchID();
-//				Check på ID her
-//					if(fail)
-//						retry?
-//							continue;
-//						else 
-//							break;
-//							reset = true;
-//					else
-//						sub_step_done = true;
-			}
-			
-			sub_step_done = false;
-			while(!reset && !state_done && !sub_step_done){
-//				double n = b.getNetto(double netto, double tolerance);
-				
-//				Kontrollere om netto overholder tolerance.
-//					if(fail)
-//						retry?
-//							continue;
-//					else
-//					sub_step_done = true;
-			}
-//			while(){ // Step hvor data gemmes.
-//			}
-			
-//			boolean quite = bound.getQuit();
-//				if(quit)
-//					DB.produktStates("afslutte");
-			
-//			Og så smid tilbage til starten.
-		}
+			while(!reset && !state_done && step7_done){
+
+				boolean quit = bound.getQuit();
+					if(quit){
+						DB.produktStatus("afslutte");
+						step7_done = false;
+					}
+			}		}
 	}
 }
